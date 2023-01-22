208d78832f1766de8170997375b04dc2b549e6c6
rmiarecki <rafal.miarecki@gmail.com>
2023-01-22 19:42:11 +0100

    comments section

--- entries/models.py
+++ entries/models.py
@@ -15,3 +15,12 @@ class Entry(models.Model):
 
     def reduction(self):
         return self.body[:280] + '...'
+    
+class Comment(models.Model):
+    entry = models.ForeignKey(Entry, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
